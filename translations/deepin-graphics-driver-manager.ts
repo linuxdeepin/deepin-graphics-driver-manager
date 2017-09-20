752a103063aa1393dda0c3e5423fb9402c5d07fa
石博文 <sbw@sbw.so>
2017-09-20 17:30:01 +0800

    add translations file

---
+++ translations/deepin-graphics-driver-manager.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="21"/>
+        <source>Toggle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="19"/>
+        <location filename="../main.cpp" line="20"/>
+        <source>Deepin Graphics Driver Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
