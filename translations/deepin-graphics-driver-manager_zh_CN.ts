c02972e09e9ab8a77ef0b07aa39128c6440106c2
石博文 <sbw@sbw.so>
2017-09-26 19:11:20 +0800

    add translation settings
    Change-Id: I3095fc2cc1190b2045502ade099ea22625af2919

---
+++ translations/deepin-graphics-driver-manager_zh_CN.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="21"/>
+        <source>Toggle</source>
+        <translation>切换</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="19"/>
+        <location filename="../main.cpp" line="20"/>
+        <source>Deepin Graphics Driver Manager</source>
+        <translation>深度显卡驱动管理器</translation>
+    </message>
+</context>
+</TS>
