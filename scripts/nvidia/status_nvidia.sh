18869f8106cc024410a2622c8948b5c0c7fb80f9
Caicai <caizp2008@163.com>
2017-10-31 19:00:45 +0800

    1.Modified single nv card detect status.
    Change-Id: I7fea293f6595277989b3d6b38a1fe197e0e27d14
    Signed-off-by: Caicai <caizp2008@163.com>

--- scripts/nvidia/status_nvidia.sh
+++ scripts/nvidia/status_nvidia.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# exit 0 ------ no driver,use default glamor
+# exit 1 ------ use open nouveau driver 
+# exit 2 ------ use private nvidia driver
 nouveau_mod=`lsmod | grep nouveau`
 nvidia_mod=`lsmod | grep nvidia`
 
 if [ -n "$nouveau_mod" ]; then
-	 echo "Had already used opensource nouveau driver"
-	 exit 1
+	if [ -f /usr/lib//xorg/modules/drivers/nouveau_drv.so ];then
+		echo "Had already used opensource nouveau driver"
+		exit 1
+	else
+		echo "Had already used default glamor driver"
+		exit 0
+	fi	
 fi
 
 
@@ -13,4 +21,5 @@ if [ -n "$nvidia_mod" ]; then
 	 echo "Had already used private nvidia driver "
 	 exit 2
 fi
+
 exit 0
