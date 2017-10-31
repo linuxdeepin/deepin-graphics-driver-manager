db56de187a3b205ac5c01a9a771238274ad41d93
石博文 <sbw@sbw.so>
2017-10-31 09:03:48 +0800

    add intel status script
    Change-Id: I6e9d6d8ddf8246bb2cb232e9aad90912b3f8ae46

---
+++ scripts/intel/intel_stat.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#当前的intel加速方式
+#intel_accel_method=`cat /var/log/intel_driver_mode.status`
+intel_accel_method_sna=`cat /var/log/Xorg.0.log | grep SNA`
+intel_accel_method_uxa=`cat /var/log/Xorg.0.log | grep UXA`
+if [ -n "$intel_accel_method_sna" ];then
+	echo "now intel accel mode is : SNA"
+	intel_accel_method=1
+	exit 1
+elif [ -n "$intel_accel_method_sna" ];then
+	echo "now intel accel mode is : UXA"
+	intel_accel_method=2
+	exit 2
+elif
+	echo "now intel accel mode is : glamor(default)"
+	intel_accel_method=0
+	exit 0
+fi
+
+# 0 ------ default is glamor
+# 1 ------ use SNA accel method
+# 2 ------ use UXA accel method
+
+set_intel_accel_sna() {
+INTEL_XORG_CONF="/etc/X11/xorg.conf.d/20-intel.conf"
+echo 'Section "Device"
+Identifier "Intel Graphics"
+Driver "intel"
+Option "AccelMethod" "sna"
+#Option "PageFlip" "False"
+#Option "TearFree" "True"
+EndSection' | sudo tee $INTEL_XORG_CONF
+}
+
+set_intel_accel_uxa() {
+INTEL_XORG_CONF="/etc/X11/xorg.conf.d/20-intel.conf"
+echo 'Section "Device"
+Identifier "Intel Graphics"
+Driver "intel"
+Option "AccelMethod" "uxa"
+#Option "PageFlip" "False"
+#Option "TearFree" "True"
+EndSection' | sudo tee $INTEL_XORG_CONF
+}
+
+#Xorg -configure
+#[ -f /root/xorg.conf.new ] && cp /root/xorg.conf.new /etc/X11/xorg.conf
+
+#根据需求调整显卡的加速方式，从而达到最优化
+case $intel_accel_method in
+	0)
+		rm /etc/X11/xorg.conf.d/20-intel.conf
+		echo "Using default glamor accel method"
+	;;
+	1)set_intel_accel_sna
+		
+	;;
+	2)set_intel_accel_uxa
+	;;
+	*)
+		rm /etc/X11/xorg.conf.d/20-intel.conf
+		echo "You need to set an accel method,default glamor"
+	;;	
+esac	
+
+#需要用户界面来提示是否保存资料，重启桌面
+#sudo systemctl restart lightdm
