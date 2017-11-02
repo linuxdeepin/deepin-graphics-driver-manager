8b593347f997e170a31d5516b0955b441111697e
石博文 <sbw@sbw.so>
2017-11-02 19:06:42 +0800

    generate ts files
    Change-Id: I86a0851f28702fad1fa8cd0cab4b0ed5aa4dcca2

---
+++ translations/deepin-dgradrimgr-gltest.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>GLTestWindow</name>
+    <message>
+        <location filename="../gltest/main.cpp" line="101"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gltest/main.cpp" line="102"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gltest/main.cpp" line="103"/>
+        <source>Please ensure the driver works normally without blurred screen and screen tearing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- translations/deepin-graphics-driver-manager.ts
+++ translations/deepin-graphics-driver-manager.ts
@@ -9,16 +9,51 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="25"/>
+        <location filename="../mainwindow.cpp" line="41"/>
         <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="202"/>
+        <source>Switching</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="204"/>
+        <source>Switching to %1, please wait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="225"/>
+        <source>Switch Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="226"/>
+        <source>Sorry, switch failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="232"/>
+        <source>Switch Succeeded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="233"/>
+        <source>The graphics driver will take effect after reboot</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.cpp" line="19"/>
-        <location filename="../main.cpp" line="20"/>
+        <location filename="../main.cpp" line="21"/>
+        <location filename="../main.cpp" line="22"/>
         <source>Deepin Graphics Driver Manager</source>
         <translation type="unfinished"></translation>
     </message>
