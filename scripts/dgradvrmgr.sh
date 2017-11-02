9b48b989ce3100d5272c559b21cb0b575b267842
石博文 <sbw@sbw.so>
2017-11-02 09:08:40 +0800

    fix config not change when post successful, because it's in overlay-root
    Change-Id: I11e598a859bfd91e0f63993eaa354f847d62155f

--- scripts/dgradvrmgr.sh
+++ scripts/dgradvrmgr.sh
@@ -22,7 +22,7 @@ if [ -e "/tmp/gltest-success" ]; then
 	#touch /tmp/succes
 #	bash -x /tmp/exe.sh post
 	/tmp/exe.sh post
-	sed -i -E 's/(success=.*$)/\1true/' /usr/lib/deepin-graphics-driver-manager/config.conf
+	overlayroot-chroot sed -i -E 's/(success=).*$/\1true/' /usr/lib/deepin-graphics-driver-manager/config.conf
 fi
 
 reboot
