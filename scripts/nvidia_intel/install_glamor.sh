14836be7a92c8acefc2f51bc357f1420adff5e55
Caicai <caizp2008@163.com>
2017-11-03 18:02:21 +0800

    1.fix switch to default mode , can not remove bumblebee.conf
    Change-Id: Iaec672ac79307632fd84cb08f0fe157097c3763c
    Signed-off-by: Caicai <caizp2008@163.com>

--- scripts/nvidia_intel/install_glamor.sh
+++ scripts/nvidia_intel/install_glamor.sh
@@ -27,9 +27,10 @@ if [ $1 == "post" ];then
 	overlayroot-chroot apt purge xserver-xorg-video-nouveau -y --allow-downgrades
 	overlayroot-chroot apt install xserver-xorg-core --reinstall -y --allow-downgrades
 	overlayroot-chroot apt install xserver-xorg-input-all --reinstall -y --allow-downgrades
-	[ -f /etc/X11/xorg.conf ] && overlayroot-chroot rm -rf /etc/X11/xorg.conf
-	[ -f /etc/modprobe.d/bumblebee.conf ] && overlayroot-chroot rm -rf /etc/modprobe.d/bumblebee.conf
-	[ -f /etc/X11/xorg.conf.d/20-nouveau.conf ] && overlayroot-chroot rm -rf /etc/X11/xorg.conf.d/20-nouveau.conf
+	overlayroot-chroot rm -rf /etc/X11/xorg.conf
+	overlayroot-chroot rm -rf /etc/modprobe.d/bumblebee.conf
+	overlayroot-chroot rm -rf /etc/X11/xorg.conf.d/20-nouveau.conf
+	overlayroot-chroot rm -rf /etc/X11/xorg.conf.d/20-nvidia.conf
 	echo "Sync driver into disk ...... done"
 else
 	if [ -n "$nvidia_mod" ]; then
