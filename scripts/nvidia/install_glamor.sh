b911ee80a37324d4c24de3195ce47b5a891748d5
listenerri <listenerri@gmail.com>
2018-11-16 15:55:22 +0800

    fix: nvidia glamor start X failed
    Change-Id: Ibc03298a6c68080ddf2b438716868ffcb0ee3859

--- scripts/nvidia/install_glamor.sh
+++ scripts/nvidia/install_glamor.sh
@@ -24,4 +24,10 @@ else
         xserver-xorg-core \
         xserver-xorg-input-all \
         libgl1-mesa-glx
+
+    # 默认驱动方案也需要让内核加载一个n卡的模块以便系统可以识别，否则系统不知道有这个物理设备
+    # 由于nouveau模块是无法通过包管理器卸载的，因此在这里加载nouveau
+    # 让内核加载nouveau模块，但不让X加载Xorg的nouveau或nvidia模块就是默认n卡方案了
+    echo "Loading kernel modules......"
+    modprobe nouveau
 fi
