04aff9e53e14f0c511af2858f53c2bbf9577f55e
haruyukilxz <lxz@ilxz.me>
2018-11-22 10:52:53 +0800

    feat: add genericName
    Change-Id: Ic6f00f435e6af447b33f3d6fd99028504c7b1dbe

---
+++ translations/desktop/desktop.ts
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1"><context><name>desktop</name><message><location filename="Desktop Entry]Comment" line="0"/><source>Deepin</source><translation type="unfinished"/></message><message><location filename="Desktop Entry]Name" line="0"/><source>Deepin Graphics Driver Manager</source><translation type="unfinished"/></message><message><location filename="Desktop Entry]GenericName" line="0"/><source>Graphics Driver Manager</source><translation type="unfinished"/></message><message><location filename="Desktop Entry]Keywords" line="0"/><source>package;install;driver;graphics</source><translation type="unfinished"/></message></context></TS>
