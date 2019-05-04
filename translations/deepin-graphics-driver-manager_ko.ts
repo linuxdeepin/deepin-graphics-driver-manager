6c095d09c1b28cf897ba99aa8c941c21d7f20d61
MarongHappy <36289819+MarongHappy@users.noreply.github.com>
2019-05-05 02:54:50 +0900

    Create deepin-graphics-driver-manager_ko.ts

---
+++ translations/deepin-graphics-driver-manager_ko.ts
@@ -0,0 +1,273 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR" sourcelanguage="en_US">
+<context>
+    <name>GLTestWindow</name>
+    <message>
+        <location filename="../gltest/main.cpp" line="124"/>
+        <source>Apply</source>
+        <translation type="unfinished">적용</translation>
+    </message>
+    <message>
+        <location filename="../gltest/main.cpp" line="125"/>
+        <source>Cancel</source>
+        <translation type="unfinished">취소</translation>
+    </message>
+    <message>
+        <location filename="../gltest/main.cpp" line="126"/>
+        <source>Please ensure the driver works normally without blurred screen and screen tearing</source>
+        <translation type="unfinished">화면이 흐려지고 화면이 끊어지지 않고 드라이버가 정상적으로 작동하는지 확인합니다</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Switch</source>
+        <translation type="unfinished">스위치</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="58"/>
+        <source>OK</source>
+        <translation type="unfinished">확인</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="62"/>
+        <source>Reboot</source>
+        <translation type="unfinished">재부팅</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Your hardware is not supported currently, please wait for future version.</source>
+        <translation type="unfinished">현재 하드웨어가 지원되지 않습니다. 다음 버전이 나올 때까지 기다려주세요.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="267"/>
+        <source>Downloading</source>
+        <translation type="unfinished">다운로드 중</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="269"/>
+        <source>Downloading the driver for %1, please wait...</source>
+        <translation type="unfinished">%1 드라이버를 다운로드하는 중입니다. 잠시 기다려 주세요...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="298"/>
+        <source>Download Successful</source>
+        <translation type="unfinished">다운로드 성공</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="300"/>
+        <source>Please reboot to test the driver.
+
+If no signal, please confirm whether the monitor output port is connected correctly.</source>
+        <translation type="unfinished">드라이버를 테스트하려면 재부팅하세요.
+
+신호가 없으면 모니터 출력 포트가 올바르게 연결되었는지 확인합니다.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="302"/>
+        <source>Please reboot to test the driver</source>
+        <translation type="unfinished">드라이버를 테스트하려면 재부팅하세요</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="290"/>
+        <source>Download failed</source>
+        <translation type="unfinished">다운로드 실패</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="38"/>
+        <source>Dark theme</source>
+        <translation type="unfinished">어두운 테마</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="291"/>
+        <source>Sorry, switch failed</source>
+        <translation type="unfinished">죄송합니다. 스위치가 실패했습니다</translation>
+    </message>
+</context>
+<context>
+    <name>Resolution</name>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="8"/>
+        <source>Use AMD card driver</source>
+        <translation type="unfinished">AMD 카드 드라이버 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="9"/>
+        <source>For special needs. Auto determine the acceleration way without external configuration.</source>
+        <translation type="unfinished">특수 요구 사항입니다. 외부 구성 없이 가속 방식을 자동으로 결정합니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="11"/>
+        <source>NVIDIA Default Driver</source>
+        <translation type="unfinished">NVIDIA 기본 드라이버</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="14"/>
+        <source>NVIDIA Open Source Driver</source>
+        <translation type="unfinished">NVIDIA 오픈 소스 드라이버</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="17"/>
+        <source>NVIDIA Closed Source Driver</source>
+        <translation type="unfinished">NVIDIA 클로즈드 소스 드라이버</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="19"/>
+        <source>Only adaptive to parts of distributions and graphics cards, may cause black screen.</source>
+        <translation type="unfinished">배포판 및 그래픽 카드의 일부에만 적응하면 화면이 검게 나타날 수 있습니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="21"/>
+        <source>Intel Default Driver</source>
+        <translation type="unfinished">인텔 기본 드라이버</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="24"/>
+        <source>Intel Compatible Mode</source>
+        <translation type="unfinished">인텔 호환 모드</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="25"/>
+        <source>Use Intel compatible mode</source>
+        <translation type="unfinished">인텔 호환 모드 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="12"/>
+        <source>Use default driver</source>
+        <translation type="unfinished">기본 드라이버 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="13"/>
+        <source>NVIDIA driver is not detected in the current environment, and NVIDIA card will not work properly.</source>
+        <translation type="unfinished">현재 환경에서 NVIDIA 드라이버가 감지되지 않으며 NVIDIA 카드가 제대로 작동하지 않습니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="15"/>
+        <source>Use open source driver</source>
+        <translation type="unfinished">오픈 소스 드라이버 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="16"/>
+        <source>Poor performance, unstable in some graphics cards.</source>
+        <translation type="unfinished">일부 그래픽 카드에서는 성능이 떨어지고 불안정합니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="18"/>
+        <source>Use closed source driver</source>
+        <translation type="unfinished">클로즈드 소스 드라이버 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="22"/>
+        <source>Use Intel default driver</source>
+        <translation type="unfinished">인텔 기본 드라이버 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="23"/>
+        <source>Poor compatibility for parts of old graphics cards.</source>
+        <translation type="unfinished">오래된 그래픽 카드의 일부와 호환되지 않습니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="26"/>
+        <source>Best compatibility for all Intel graphics cards to accelerate, but performance is relatively weak.</source>
+        <translation type="unfinished">모든 인텔 그래픽 카드에 대한 최상의 호환성은 가속화되지만 성능은 상대적으로 약합니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="27"/>
+        <source>Intel Acceleration Mode</source>
+        <translation type="unfinished">인텔 가속 모드</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="28"/>
+        <source>Use Intel acceleration mode</source>
+        <translation type="unfinished">인텔 가속 모드 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="29"/>
+        <source>Speed up the graphics card with better performance, but unstable in some cards with screen flashing.</source>
+        <translation type="unfinished">그래픽 카드의 성능을 향상시키지만 화면 깜박임이 있는 카드에서는 불안정합니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="31"/>
+        <source>Use Bumblebee Solution</source>
+        <translation type="unfinished">범블비 솔루션 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="32"/>
+        <source>Bumblebee Solution</source>
+        <translation type="unfinished">범블비 솔루션</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="33"/>
+        <source>Save power to reduce power consumption.</source>
+        <translation type="unfinished">전력을 절약하여 전력 소비를 줄입니다.</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="35"/>
+        <source>Use NV-PRIME Solution</source>
+        <translation type="unfinished">NV-PRIME 솔루션 사용</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="36"/>
+        <source>PRIME Solution</source>
+        <translation type="unfinished">PRIME 솔루션</translation>
+    </message>
+    <message>
+        <location filename="../resolutions/resolution.cpp" line="37"/>
+        <source>For laptops with hybrid graphics, having best performance but must work with closed-source driver.</source>
+        <translation type="unfinished">하이브리드 그래픽이 장착된 노트북의 경우 최상의 성능을 발휘하지만 클로즈드 소스 드라이버로 작동해야합니다.</translation>
+    </message>
+</context>
+<context>
+    <name>ResolutionWidget</name>
+    <message>
+        <location filename="../widgets/resolutionwidget.cpp" line="118"/>
+        <source>Version: </source>
+        <translation type="unfinished">버전: </translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../installer/main.cpp" line="37"/>
+        <source>Congratulations, you have switched to %1, please reboot to take effect.</source>
+        <translation type="unfinished">축하합니다. %1로 전환했습니다. 적용하려면 재부팅하세요.</translation>
+    </message>
+    <message>
+        <location filename="../installer/main.cpp" line="40"/>
+        <source>Cancel</source>
+        <translation type="unfinished">취소</translation>
+    </message>
+    <message>
+        <location filename="../installer/main.cpp" line="41"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../installer/main.cpp" line="58"/>
+        <source>Auto restore to %2 after failed to switch to %1</source>
+        <translation type="unfinished">%1로 전환하지 못한 후 %2로 자동 복원</translation>
+    </message>
+    <message>
+        <location filename="../installer/main.cpp" line="61"/>
+        <source>Submit Feedback</source>
+        <translation type="unfinished">피드백 제출</translation>
+    </message>
+    <message>
+        <location filename="../installer/main.cpp" line="69"/>
+        <source>Updating the driver, please wait...</source>
+        <translation type="unfinished">드라이버를 업데이트하는 중입니다. 잠시 기다려 주세요...</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="25"/>
+        <source>Deepin Graphics Driver Manager</source>
+        <translation type="unfinished">Deepin 그래픽 드라이버 관리자</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="26"/>
+        <source>Deepin Graphics Driver Manager is a compact and easy to use graphics driver management tool. It includes graphics card hardware detection, graphics driver installation, graphics driver solution switching,  graphics driver automatic recovery and other functions.</source>
+        <translation type="unfinished">Deepin 그래픽 드라이버 관리자는 작고 사용하기 쉬운 그래픽 드라이버 관리 도구입니다. 여기에는 그래픽 카드 하드웨어 감지, 그래픽 드라이버 설치, 그래픽 드라이버 솔루션 전환, 그래픽 드라이버 자동 복구 및 기타 기능이 포함됩니다.</translation>
+    </message>
+</context>
+</TS>
