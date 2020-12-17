9938e08dea3a5a237dc541e3973f7a48e5f974a6
huxiaodong <huxiaodong@uniontech.com>
2020-12-17 16:04:53 +0800

    fix: real install阶段如果后段脚本执行失败不重启，而是发送错误码给前端，前端在给出提示信息

--- server/scripts/dgradvrmgr-real-install.sh
+++ server/scripts/dgradvrmgr-real-install.sh
@@ -7,8 +7,7 @@ journalctl -f -u dgradvrmgr-test-gltest.service | sed 's/$/\r/g' > /dev/tty1 2>&
 
 . /usr/lib/deepin-graphics-driver-manager/common.sh
 #remove/install drivers
-$REMOVE_OLD_G || error_reboot "test remove old driver failed!"
-$INSTALL_NEW_G || error_reboot "test install new driver failed!"
+$REMOVE_OLD_G || error_exit_dgm "test remove old driver failed!" 1
+$INSTALL_NEW_G || error_exit_dgm "test install new driver failed!" 1
 
 exit 0
-
