e278c79cbf6dfe821f24f8f1129dc0d79346d5e6
huxiaodong <huxiaodong@uniontech.com>
2021-03-18 20:18:08 +0800

    fix: 解决准备界面取消时，切换环境未清理干净问题
    在提示重启界面，点击取消按钮，手动重启后会提示切换失败
    
    Log: 在提示重启界面，点击取消按钮，手动重启后会提示切换失败
    
    Bug: https://pms.uniontech.com/zentao/bug-view-67362.html
         https://pms.uniontech.com/zentao/bug-view-67360.html
    Change-Id: I43d7a69359fae573b7b2a96c41098c97d1fcae56

--- server/scripts/dgradvrmgr-cancel.sh
+++ server/scripts/dgradvrmgr-cancel.sh
@@ -4,18 +4,14 @@
 
 cleanWorking
 
-if [[ -n "${OVERLAY_LOWDIR}/${isInOverlayRoot}" ]]; then
+if [[ -n "${isInOverlayRoot}" ]]; then
     [ -e "${OVERLAY_LOWDIR}/${INSTALLER_DESKTOP_FILE_DEST}" ] && /usr/sbin/overlayroot-chroot rm -rf $INSTALLER_DESKTOP_FILE_DEST
     [ -e "${OVERLAY_LOWDIR}/${TEST_INSTALLER_DESKTOP_FILE_DEST}" ] && /usr/sbin/overlayroot-chroot rm -rf $TEST_INSTALLER_DESKTOP_FILE_DEST
-    [ -e "${OVERLAY_LOWDIR}/${GLTEST_FLAG}" ] && /usr/sbin/overlayroot-chroot rm -rf $GLTEST_FLAG
-    [ -e "${OVERLAY_LOWDIR}/${OVERLAYROOT_IMAGE}" ] && /usr/sbin/overlayroot-chroot rm -rf $OVERLAYROOT_IMAGE
     nvidia_blacklist_recovery
 else
     [ -e "${INSTALLER_DESKTOP_FILE_DEST}" ] && rm -rf $INSTALLER_DESKTOP_FILE_DEST
     [ -e "${TEST_INSTALLER_DESKTOP_FILE_DEST}" ] && rm -rf $TEST_INSTALLER_DESKTOP_FILE_DEST
-    [ -e "${GLTEST_FLAG}" ] && rm -rf $GLTEST_FLAG
-    [ -e "${OVERLAYROOT_IMAGE}" ] && rm -rf $OVERLAYROOT_IMAGE
-    update-initramfs -u
+    #update-initramfs -u
 fi
 
 exit 0
