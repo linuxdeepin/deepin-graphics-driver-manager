8777058d13d84c819316f6b33789dc60a4e8fd69
huxiaodong <huxiaodong@uniontech.com>
2021-05-24 14:29:03 +0800

    feat: 给小茶壶测试程序增加log
    给小茶壶测试程序增加log
    
    Log: 给小茶壶测试程序增加log
    Change-Id: Idcd487e5cc8051132b4d2aa7f202ba3d9605ee03

--- gltest/main.cpp
+++ gltest/main.cpp
@@ -7,6 +7,10 @@
 #include "gltestwidget.h"
 #include "gltestwindow.h"
 
+#include <DLog>
+
+DCORE_USE_NAMESPACE
+
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
@@ -16,6 +20,20 @@ int main(int argc, char *argv[])
     QTranslator trans;
     trans.load(QString(TRANSLATIONS_DIR"/deepin-graphics-driver-manager_%1.qm").arg(lang));
     app.installTranslator(&trans);
+
+    //设置日志
+    const QString logFormat = "%{time}{yyyyMMdd.HH:mm:ss.zzz}[%{type:1}][%{function:-40} %{line:-4} %{threadid:-8} ] %{message}\n";
+    const QString log_file(QString("/var/log/deepin-graphics-driver-manager-server.log"));
+    ConsoleAppender *consoleAppender = new ConsoleAppender;
+    consoleAppender->setFormat(logFormat);
+    RollingFileAppender *rollingFileAppender = new RollingFileAppender(log_file);
+    rollingFileAppender->setFormat(logFormat);
+    rollingFileAppender->setLogFilesLimit(5);
+    rollingFileAppender->setDatePattern(RollingFileAppender::DailyRollover);
+
+    logger->registerAppender(consoleAppender);
+    logger->registerAppender(rollingFileAppender);
+
     GLTestWindow w;
     w.show();
 
